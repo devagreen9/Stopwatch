5e5bc0f9504f96dfd73b9da63ea6daf3e0ffd65a
devagreen9 <61478502+devagreen9@users.noreply.github.com>
2020-03-28 20:05:24 -0400

    Add files via upload

--- stopwatch/src/App.js
+++ stopwatch/src/App.js
@@ -1,46 +1,58 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-export default class App extends Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      color: 'purple',
-      clickCounter: 0
-    }
-  }
- 
-  onChange = (e) => {
-    this.setState({
-      color: 'orange',
-      clickCounter: this.state.clickCounter + 1
-    })
-    console.log('click counter ===>', this.state.clickCounter)
-  }
-  
-  onNextChange = (e) => {
-    this.setState({
-      color: 'purple',
-      clickCounter: this.state.clickCounter + 1
-    })
-    console.log('click counter ===>', this.state.clickCounter)
+      time: 0,
+      clearId: null
+    };
   }
 
+  this.timer = this.timer.bind(this);
+  this.startTimer = this.startTimer.bind(this);
+
+  timer(e) {  // e = event
+    e.preventDefault(); // stops the page from refreshing 
+    const buttonClicked = e.target.id;
+    // console.log(buttonClicked);
+    if(buttonClicked === 'start') {
+      setInterval(this.startTimer, 1000);
+    } else if(buttonClicked === 'reset') {
+      this.setState({ time: 0});
+    } else if(buttonClicked === 'pause') {
+      clearId;
+    }
+  }
+
+  // start function -> when button click I have to increase seconds/time
+      // how do I increase time
+      // setInterval() to be invoked onClick
+  startTimer() {
+
+    function add() {
+      this.setState({ time: state.time + 1 }) 
+    }
+    setInterval(this.setState({ time: this.state.time + 1}), 1000);
+  }
 
   render() {
     return (
-      <div className='App'>
-        <h1>Box Color Change</h1>
-        <div className='colorBox' style={{backgroundColor: this.state.color}}
-        
-        >
-        </div>
-        <div className='buttons'>
-          <button onClick={this.onChange} >Click Me!</button>
-          <button onClick={this.onNextChange}>No! Click Me!</button>
-        </div>
+      <div className="App">
+        <header className="App-header">
+    <h1>{  }</h1>
+          <form onClick={ this.timer }>
+            <button id='start'>Start</button>
+            <button id='pause'>Pause</button>
+            <button id='reset'>Reset</button>
+          </form>
+        </header>
       </div>
-    )
+    );
   }
-}
+  }
+  
+
+export default App;
